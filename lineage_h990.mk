1572f42ee50f5e6c6c1299b4a1e0b2813541b0dc
carpe462 <carpe462@gmail.com>
2020-07-18 01:45:58 -0700

    syberia

--- lineage_h990.mk
+++ lineage_h990.mk
@@ -18,8 +18,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Syberia stuff
+$(call inherit-product, vendor/syberia/config/common.mk)
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+SYBERIA_BUILD_TYPE := UNOFFICIAL
 
 # Inherit from h990 device
 $(call inherit-product, device/lge/h990/device.mk)
